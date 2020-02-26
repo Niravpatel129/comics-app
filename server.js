aef15e1364dbc1d6d4c5cf33bf36847c37bca134
Niravpatel129 <niravpatelp129@gmail.com>
2020-02-26 13:05:52 -0500

    all functions of the app added

--- server.js
+++ server.js
@@ -2,27 +2,67 @@ const express = require("express");
 const app = express();
 const axios = require("axios");
 
-app.use(express.static("public"));
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + "/public"));
 app.engine("html", require("ejs").renderFile);
 app.set("view engine", "html");
 
+// helper function
+let pageVisitCount = {};
+
+// hold all the page visits for each comic
+function incrementPageVisit(id) {
+  if (pageVisitCount[id]) {
+    pageVisitCount = {
+      ...pageVisitCount,
+      [id]: (pageVisitCount[id] += 1)
+    };
+  } else {
+    pageVisitCount = { ...pageVisitCount, [id]: 1 };
+  }
+}
+
+function getHighestVisitPage() {
+  if (Object.keys(pageVisitCount).length === 0) {
+    // just return 1 if there is no visits yet
+    return 1;
+  }
+
+  const arr = Object.keys(pageVisitCount).map(function(key) {
+    return key;
+  });
+
+  return Math.max.apply(null, arr);
+}
+
 //routes
 const router = express.Router();
+
 router.get("/", function(req, res) {
-  res.send("hello world");
+  res.redirect(`/comic/`);
 });
 
-router.get("/comic/:id", async function(req, res) {
+router.get("/comic/:id?", async function(req, res) {
   let data = {};
-  await axios.get(`https://xkcd.com/${req.params.id}/info.0.json`).then(res => {
-    data = res.data;
-  });
+  incrementPageVisit(req.params.id);
+  await axios
+    .get(`https://xkcd.com/${req.params.id || ""}/info.0.json`)
+    .then(res => {
+      data = res.data;
+    });
+
+  data = { ...data, pageVisitCount: pageVisitCount[req.params.id] };
   res.render("comic", data);
+});
+
+// catch all undefined pages
+router.get("*", (req, res) => {
+  res.send("Error page not found!");
 });
 
 //add the router
 app.use("/", router);
-const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log("app running on ", PORT);
